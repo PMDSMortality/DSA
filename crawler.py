729e50b6d3eb476422112d575b347f6a64be3042
Steffen Bartsch <steff.bartsch@gmx.de>
2024-04-19 12:47:15 +0200

    extracted all happenings date filtered as json

--- crawler.py
+++ crawler.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import pandas
 import time
+from datetime import datetime
+import pandas as pd
+import re
 
 def crawl_page_lazy_loading(save_as_file=False):
     chrome = webdriver.Chrome()
@@ -37,19 +39,75 @@ def crawl_page_lazy_loading(save_as_file=False):
     entries = soup.find_all("div", {"class": "c-component"})
     return entries
 
-def extract_from_entries(entries, elem_to_find):
+def extract_from_entries(entries: list, elements_to_find: list):
     collected_entries = []
 
     for entry in entries:
-        elements = entry.find_all(elem_to_find, recursive=True)
-        for x in elements:
-            text =  x.getText()
-            # text = text.replace(" ", "")
-            text = text.replace("\n", "")
-            text = text.strip()
-            collected_entries.append(text)
-        
+        finds = []
+        for tag in elements_to_find:
+            found_elements = entry.find_all(tag, recursive=True)
+            for x in found_elements:
+                text =  x.getText()
+                text = text.replace("  ", "")
+                text = text.replace("\n", "")
+                text = text.strip()
+                if text:
+                    finds.append(text)
+
+        if len(finds) > 0:                    
+            finds = list(dict.fromkeys(finds))
+            collected_entries.append(finds)
+
+    print(collected_entries)
     return collected_entries
 
+def get_components_from_local_html(tag_type: str, class_name = ""):
+    # read from local stored html (result of crawler)
+    HTMLFile = open("data/crawler_output.html", "r") 
+    index = HTMLFile.read() 
+    # Creating a BeautifulSoup object and specifying the parser 
+    soup = BeautifulSoup(index, 'html.parser') 
+    # print(S.body)
 
+    if class_name:
+        entries = soup.find_all(tag_type, {"class": class_name})
+    else:
+        entries = soup.find_all(tag_type)
+
+    return entries
+
+def extract_happenings_as_json(extracted_elements: list):
+    months = {
+        "Januar": "01",
+        "Februar": "02",
+        "März": "03",
+        "April": "04",
+        "Mai": "05",
+        "Juni": "06",
+        "Juli": "07",
+        "August": "08",
+        "September": "09",
+        "Oktober": "10",
+        "November": "11",
+        "Dezember": "12",
+    }
+
+    keys = months.keys()
+    results_list = []
+
+    for elem in extracted_elements:
+        date_str = elem[0]
+        # print(len(elem))
+        regex = r"^\d{1,2}. \w* \d{,4}"
+        new_list_elem = elem.copy()
+
+        if re.match(regex, date_str): 
+            date_str = date_str.replace(".", "").replace(" ", "-")
+            for key in keys:
+                date_str = date_str.replace(key, months[key])
+            date_format = '%d-%m-%Y'
+            new_list_elem[0] = date_str
+            results_list.append(new_list_elem)
     
+    return results_list
+        
